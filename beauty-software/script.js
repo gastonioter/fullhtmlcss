cef1f80e399abad8ee8bebf42e522a10d2aa9a04
gastonioter <iotting.castro10@gmail.com>
2024-03-18 22:22:53 -0300

    beauty-sw:better scrolling

--- beauty-software/script.js
+++ beauty-software/script.js
@@ -1,9 +1,21 @@
 const headerEl = document.querySelector(".header");
-
+const navListEl = document.querySelector(".nav__items");
+const sections = document.querySelectorAll("section");
 document.addEventListener("scroll", handleScroll);
+navListEl.addEventListener("click", handleNavigationClick);
 
+function handleNavigationClick(e) {
+  e.preventDefault();
+  const link = e.target;
+  if (!link.classList.contains("nav__link")) return;
+
+  const { top } = document
+    .querySelector(`section.${link.attributes.href.value.split("#").at(1)}`)
+    .getBoundingClientRect();
+
+  scrollTo(scrollX, scrollY + top - 72);
+}
 function handleScroll() {
-  console.log(scrollY);
-  if(scrollY > 72) headerEl.classList.add('header--scrolled')
+  if (scrollY > 72) headerEl.classList.add("header--scrolled");
   else headerEl.classList.remove("header--scrolled");
 }
