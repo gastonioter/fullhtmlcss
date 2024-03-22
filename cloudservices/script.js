94e93a034ae484b83227f95586559668dd1736f3
gastonioter <iotting.castro10@gmail.com>
2024-03-21 21:45:48 -0300

    add register form

--- cloudservices/script.js
+++ cloudservices/script.js
@@ -18,3 +18,19 @@ const footerObserver = new IntersectionObserver(
 );
 
 footerObserver.observe(footer);
+
+document.querySelector("#register").addEventListener("submit", handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault();
+  // console.log(e.target.name.value);
+  // const languajes = [];
+  // e.target.languajes.forEach((checkbox) => {
+  //   checkbox.checked ? languajes.push(checkbox.value) : "";
+  // });
+
+  // e.target.submit();
+  // console.log(e.target.live.formAction);
+
+  const formData = new FormData(document.querySelector("#register"));
+  console.log(formData);
+}
